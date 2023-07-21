aa513b90bac995a1f3b8eb66cf3227087c8b7a3d
kjsheehy <kjsheehy@gmail.com>
2023-07-21 13:03:28 -0400

    Fixed styling issues on intro modal

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -8,7 +8,7 @@ function Intro(props) {
         Welcome to DC Run Routes, v1! This site showcases beautiful running
         routes in and around DC. There's some info about each route, including
         distance, location, and surface type, to help you find a great run for
-        you. There are also a number of photos taken along each route to
+        you. There are also a number of photos I took along each route to
         highlight the scenary you can expect to enjoy. Happy running!
       </p>
       <p className="page-signature">-Kevin</p>
