313dccbf7a81dff632af8140496758c593978d9e
kjsheehy <kjsheehy@gmail.com>
2023-08-21 17:18:51 -0400

    Route link placeholders added to route modal

--- client/src/components/Routes/RouteLinks.js
+++ client/src/components/Routes/RouteLinks.js
@@ -1,5 +1,14 @@
 import './RouteLinks.css';
 
 export default function RouteLinks(props) {
-  return <div>Route links here</div>;
+  return (
+    <div className="route-links">
+      <a href="http://google.com/maps">Google Maps</a>
+      <div className="route-apps">
+        <a href="http://strava.com">Strava</a>
+        <a href="http://mapmyrun.com">MapMyRun</a>
+        <a href="http://garmin.com">Garmin</a>
+      </div>
+    </div>
+  );
 }
