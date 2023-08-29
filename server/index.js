81dcdbecf4c34a75a0155ed5615c1c37a385cf67
kjsheehy <kjsheehy@gmail.com>
2023-08-29 13:46:41 -0400

    Cleaned up route data in preparation for implementation of mongodb

--- server/index.js
+++ server/index.js
@@ -20,38 +20,38 @@ app.use(
   express.static(path.join(__dirname, '..', 'client', 'build'))
 );
 
-// const mongoUri =
-//   'mongodb+srv://kjsheehy:Punch93Seventeen@dc-run-routes.jp0qhli.mongodb.net/?retryWrites=true&w=majority';
+const mongoUri =
+  'mongodb+srv://kjsheehy:Punch93Seventeen@dc-run-routes.jp0qhli.mongodb.net/?retryWrites=true&w=majority';
 
-// const mongoClient = new MongoClient(mongoUri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   },
-// });
+const mongoClient = new MongoClient(mongoUri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
-// async function runMongo() {
-//   try {
-//     await mongoClient.connect();
-//     await mongoClient.db('dc-run-routes').command({ ping: 1 });
-//     console.log('Pinged your deployment. Successfully connected to MongoDB!');
-//   } finally {
-//     await mongoClient.close();
-//   }
-// }
-// runMongo().catch(console.dir);
+async function runMongo() {
+  try {
+    await mongoClient.connect();
+    await mongoClient.db('dc-run-routes').command({ ping: 1 });
+    console.log('Pinged your deployment. Successfully connected to MongoDB!');
+  } finally {
+    await mongoClient.close();
+  }
+}
+runMongo().catch(console.dir);
 
-// // const result = await db.collection('routes').insertMany(routes);
-// //db.collection('routes').insertMany(routes);
+// const result = await db.collection('routes').insertMany(routes);
+//db.collection('routes').insertMany(routes);
 
-// async function createRoutes(newRoutes) {
-//   const result = await mongoClient
-//     .db('dc-run-routes')
-//     .collection('routes')
-//     .insertMany(newRoutes);
-//   console.log(`${result.insertedCount} new listings created`);
-// }
+async function createRoutes(newRoutes) {
+  const result = await mongoClient
+    .db('dc-run-routes')
+    .collection('routes')
+    .insertMany(newRoutes);
+  console.log(`${result.insertedCount} new listings created`);
+}
 
 const urlBase = '/dc-run-routes/api';
 
@@ -59,9 +59,8 @@ const routes = [
   {
     id: 0,
     name: 'Georgetown Waterfront Bridges',
-    thumbnailSrc: `./KeyBridgeEast.jpg`,
-    thumbnailIsPortrait: false,
     photos: [
+      `./KeyBridgeEast.jpg`,
       `./KeyBridgeSunrise.jpeg`,
       './GeorgetownWaterfrontParkPath.jpeg',
       './RooseveltBridgeRosslyn.jpeg',
@@ -71,7 +70,6 @@ const routes = [
       './RockCreekTrailRosslyn.jpeg',
     ],
     distance: 3.4,
-    extendable: false,
     location: 'N Arlington',
     surface: ['Paved Path', 'Sidewalk'],
     features: ['Flat'],
@@ -82,25 +80,39 @@ const routes = [
   {
     id: 1,
     name: 'Teddy Roosevelt Island',
-    thumbnailSrc: `./TeddyRooseveltInteriorLushDeer.jpeg`,
     distance: 2.0,
-    extendable: false,
     location: 'N Arlington',
     surface: ['Trail', 'Boardwalk'],
     features: ['Flat'],
     type: 'Loop',
+    photos: [
+      './TeddyRooseveltInteriorLushDeer.jpeg',
+      './KeyBridgeEast.jpg',
+      './WindyRunPotomacEastSunriseFinn.jpeg',
+      './HainsPointSWSunset.jpeg',
+      './ArboretumCreek.jpeg',
+      './OldTownWaterfrontTownhouses.jpeg',
+      './KeyBridgeEast.jpg',
+    ],
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
   },
   {
     id: 2,
     name: 'Windy Run',
-    thumbnailSrc: `./WindyRunPotomacEastSunriseFinn.jpeg`,
     distance: 1.5,
-    extendable: true,
     location: 'N Arlington',
     surface: ['Trail'],
     features: ['Rocky'],
+    photos: [
+      './WindyRunPotomacEastSunriseFinn.jpeg',
+      './KeyBridgeEast.jpg',
+      './TeddyRooseveltInteriorLushDeer.jpeg',
+      './HainsPointSWSunset.jpeg',
+      './ArboretumCreek.jpeg',
+      './OldTownWaterfrontTownhouses.jpeg',
+      './KeyBridgeEast.jpg',
+    ],
     type: 'Out & Back',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
@@ -110,7 +122,15 @@ const routes = [
     name: 'Hains Point',
     thumbnailSrc: `./HainsPointSWSunset.jpeg`,
     distance: 2.9,
-    extendable: false,
+    photos: [
+      './HainsPointSWSunset.jpeg',
+      './KeyBridgeEast.jpg',
+      './TeddyRooseveltInteriorLushDeer.jpeg',
+      './WindyRunPotomacEastSunriseFinn.jpeg',
+      './ArboretumCreek.jpeg',
+      './OldTownWaterfrontTownhouses.jpeg',
+      './KeyBridgeEast.jpg',
+    ],
     location: 'SW D.C.',
     surface: ['Road', 'Sidewalk'],
     features: ['Flat'],
@@ -121,12 +141,18 @@ const routes = [
   {
     id: 4,
     name: 'National Arboretum',
-    thumbnailSrc: `./ArboretumCreek.jpeg`,
-    thumbnailIsPortrait: true,
     distance: 3.0,
-    extendable: true,
     location: 'NE D.C.',
     surface: ['Road'],
+    photos: [
+      './ArboretumCreek.jpeg',
+      './KeyBridgeEast.jpg',
+      './TeddyRooseveltInteriorLushDeer.jpeg',
+      './WindyRunPotomacEastSunriseFinn.jpeg',
+      './HainsPointSWSunset.jpeg',
+      './OldTownWaterfrontTownhouses.jpeg',
+      './KeyBridgeEast.jpg',
+    ],
     features: ['Big Hills'],
     type: 'Loop',
     description:
@@ -137,10 +163,18 @@ const routes = [
     name: 'Old Town Waterfront',
     thumbnailSrc: `./OldTownWaterfrontTownhouses.jpeg`,
     distance: 1.0,
-    extendable: true,
     location: 'Alexandria',
     surface: ['Paved Path'],
     features: ['Flat'],
+    photos: [
+      './OldTownWaterfrontTownhouses.jpeg',
+      './KeyBridgeEast.jpg',
+      './TeddyRooseveltInteriorLushDeer.jpeg',
+      './WindyRunPotomacEastSunriseFinn.jpeg',
+      './HainsPointSWSunset.jpeg',
+      './ArboretumCreek.jpeg',
+      './KeyBridgeEast.jpg',
+    ],
     type: 'Out & Back',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
@@ -148,9 +182,16 @@ const routes = [
   {
     id: 6,
     name: 'Key + Memorial Bridges Loop',
-    thumbnailSrc: `./KeyBridgeEast.jpg`,
     distance: 4.4,
-    extendable: true,
+    photos: [
+      './KeyBridgeSunrise.jpeg',
+      './TeddyRooseveltInteriorLushDeer.jpeg',
+      './WindyRunPotomacEastSunriseFinn.jpeg',
+      './HainsPointSWSunset.jpeg',
+      './ArboretumCreek.jpeg',
+      './OldTownWaterfrontTownhouses.jpeg',
+      './KeyBridgeEast.jpg',
+    ],
     location: 'N Arlington',
     surface: ['Paved Path', 'Sidewalk'],
     features: ['Flat'],
@@ -160,7 +201,15 @@ const routes = [
   },
 ];
 
-const testImages = routes.map((route) => route.thumbnailSrc);
+const testImages = [
+  './KeyBridgeEast.jpg',
+  './TeddyRooseveltInteriorLushDeer.jpeg',
+  './WindyRunPotomacEastSunriseFinn.jpeg',
+  './HainsPointSWSunset.jpeg',
+  './ArboretumCreek.jpeg',
+  './OldTownWaterfrontTownhouses.jpeg',
+  './KeyBridgeEast.jpg',
+];
 
 app.get(`/dc-run-routes/`, (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
