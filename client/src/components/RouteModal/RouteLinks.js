13ce1e000feabea894a75e449f05d08461b029ec
kjsheehy <kjsheehy@gmail.com>
2023-09-24 17:28:34 -0400

    Removal of route description from route modal and rename of Google Maps link

--- client/src/components/RouteModal/RouteLinks.js
+++ client/src/components/RouteModal/RouteLinks.js
@@ -3,7 +3,7 @@ import './RouteLinks.css';
 export default function RouteLinks(props) {
   return (
     <div className="route-links">
-      <a href={props.maps}>Google Maps</a>
+      <a href={props.maps}>Directions to Start</a>
       <div className="route-apps">
         <a href={props.strava}>Strava</a>
         <a href={props.mapMyRun}>MapMyRun</a>
