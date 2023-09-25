78a010ebf3448459b177bd63de3005303d5db397
kjsheehy <kjsheehy@gmail.com>
2023-09-25 10:56:04 -0400

    Removal of placeholder data message in the intro modal...becuase it's all real data now

--- client/src/components/UI/Intro.js
+++ client/src/components/UI/Intro.js
@@ -5,18 +5,10 @@ function Intro(props) {
   return (
     <Modal close={props.close} className="page-intro">
       <p className="page-description">
-        Welcome to DC Run Routes, v1! This site showcases beautiful running
-        routes in and around DC.{' '}
-        {/*There's some info about each route, including
+        Welcome to DC Run Routes! There's some info about each route, including
         distance, location, and surface type, to help you find a great run for
         you. There are also a number of photos I took along each route to
-        highlight the scenary you can expect to enjoy. Happy running! */}
-      </p>
-      <p></p>
-      <p className="page-description">
-        Note that most of the info, images, and links on the route detail page
-        are currently just placeholders for development and demo purposes. More
-        to come!
+        highlight the scenary you can expect to enjoy. Happy running!
       </p>
       <p className="page-signature">-Kevin</p>
     </Modal>
